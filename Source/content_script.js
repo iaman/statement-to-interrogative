f1497a434b84a050515cb51564d1e58be82b89a1
Ian O'Dea <ianodea@gmail.com>
2016-01-19 00:25:32 -0600

    Cloud to Clown

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,11 +31,14 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bThe Cloud\b/g, "The Clown");
+	v = v.replace(/\bThe cloud\b/g, "The clown");
+	v = v.replace(/\bthe Cloud\b/g, "the Clown");
+	v = v.replace(/\bthe cloud\b/g, "the clown");
+	v = v.replace(/\bCloud Computing\b/g, "Clown Computing");
+	v = v.replace(/\bCloud computing\b/g, "Clown computing");
+	v = v.replace(/\bcloud Computing\b/g, "clown Computing");
+	v = v.replace(/\bcloud computing\b/g, "cloud computing");
 	textNode.nodeValue = v;
 }
 
